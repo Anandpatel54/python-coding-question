adb75a5e974a28e70a12b6e143f610e40e20b84f
Anand Patel <panand054@gmail.com>
2025-07-25 14:48:01 +0530

    loop part making

---
+++ loop-part.py
@@ -0,0 +1,15 @@
+#(1:-) write a program to find the sum of all even numbers up to 50
+
+# sum = 0
+# for i in range(0,51):
+#     if i % 2 == 0:
+#         sum += i
+# print("The sum of all even numbers up to 50 is:", sum)
+
+#(2:-) write a program to find the sum of all odd numbers up to 50
+
+# sum = 0
+# for i in range(0, 51):
+#     if i % 2 != 0:
+#         sum += i
+# print("The sum of all odd numbers up to 50 is:", sum)
